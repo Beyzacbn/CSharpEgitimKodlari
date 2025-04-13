8b8960989716a179bc895b89e4b0fa5160ca4a82
Beyzacbn <beyzacbn@gmail.com>
2025-04-14 01:37:30 +0300

    CSharp Egitim Kodlari

---
+++ CSharpEgitimKampi501/CSharpEgitimKampi501/Form1.cs
@@ -0,0 +1,98 @@
+﻿using CSharpEgitimKampi501.Dtos;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi501
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection("Server=DESKTOP-7L0UFKL;initial Catalog=EgitimKampi501Db;integrated security=true");
+
+       
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string query = "Select * From TblProduct";
+            var values = await connection.QueryAsync<ResultProductDto>(query);
+            dataGridView1.DataSource= values;
+        }
+
+        private async void btnAdd_Click(object sender, EventArgs e)
+        {
+            string query = "insert into TblProduct (ProductName,ProductStock,ProductPrice,ProductCategory) values\r\n                (@productName,@productStock,@productPrice,@productCategory)";
+
+            var parameters = new DynamicParameters();
+            parameters.Add("@productName", txtProductName.Text);
+            parameters.Add("@productStock",txtProductStock.Text);
+            parameters.Add("@productPrice", txtProductPrice.Text);
+            parameters.Add("@productCategory", txtProductCategory.Text);
+            await connection.ExecuteAsync(query,parameters);
+            MessageBox.Show("Yeni Kitap Ekleme İşlemi Başarılı");
+
+
+        }
+
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            string query = "Delete From TblProduct Where ProductId=@productId";
+            var parameters= new DynamicParameters();
+            parameters.Add("@productId",txtProductId.Text);
+            await connection.ExecuteAsync(query, parameters);
+            MessageBox.Show("Kitap Silme İşlemi Başarılı");
+        }
+
+        private async void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string query="update TblProduct Set ProductName=@productName,ProductPrice=@productPrice,ProductStock=@productStock,ProductCategory=@productCategory where ProductId=@productId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@productName", txtProductName.Text);
+            parameters.Add("@productStock", txtProductStock.Text);
+            parameters.Add("@productPrice", txtProductPrice.Text);
+            parameters.Add("@productCategory", txtProductCategory.Text);
+            parameters.Add("@productId",txtProductId.Text);
+            await connection.ExecuteAsync(query,parameters);
+            MessageBox.Show("Kitap Güncelleme İşlemi Başarılı","Güncelleme",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+
+
+
+
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            string query1 = "Select Count(*) From TblProduct";
+            var productTotalCount= await connection.QueryFirstOrDefaultAsync<int>(query1);
+            lblTotalProductCount.Text = productTotalCount.ToString();
+
+            string query2= "Select ProductName From TblProduct where ProductPrice =(Select Max(ProductPrice) From TblProduct)";
+            var maxPriceProductName = await connection.QueryFirstOrDefaultAsync<string>(query2);
+            lblMaxPriceProduct.Text = maxPriceProductName.ToString();
+
+            string query3 = "Select Count(Distinct(ProductCategory)) From TblProduct";
+            var distinctProductCategory = await connection.QueryFirstOrDefaultAsync<int>(query3);
+            lblDistinctCategoryCount.Text = distinctProductCategory.ToString();
+        }
+    }
+}
